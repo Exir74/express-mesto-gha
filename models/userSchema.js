795c1a54c4d11b0b1259dbf46c4940512cb8d2bd
Exir74 <exir74@gmail.com>
2023-06-18 20:10:13 +0500

    create userSchema & cardSchema

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,23 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    required: true,
+  },
+  about: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    required: true,
 
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
 });
+module.exports = mongoose.model('user', userSchema);
